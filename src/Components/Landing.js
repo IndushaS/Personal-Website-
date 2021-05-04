f09b1aed50b1ed2c582cec27422e9d6ef0857d8d
Indusha <im2semba@uwaterloo.ca>
2021-05-04 19:31:21 -0400

    links with destinations

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,39 +1,50 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import profile from '../assets/profile.png'
-import {FaLinkedin, FaEnvelope, FaFile, FaGithub} from "react-icons/fa"
-import {IconContext} from "react-icons"
+import { FaLinkedin, FaEnvelope, FaFile, FaGithub } from "react-icons/fa"
+import { IconContext } from "react-icons"
 
 export default function Landing({ title, subtitle, dark, id }) {
   return (
     <div className={"section" + (dark ? " section-dark" : "")}>
       <div className="section-content" id={id}>
         <Container>
-          {/* Stack the columns on mobile by making one full-width and the other half-width */}
           <Row className="align-items-center">
             <Col xs={12} md={{ span: 7, offset: -7 }}>
               <h1>
-                Hello 
+                Hello
                 <span role="img" aria-label="">👋</span>
               </h1>
               <h2>
                 I'm Indusha Semba
               </h2>
               <p>
-                Software developer 
+               Former SWE intern @Mattermost | 4A Management Engineering @UWaterloo
               </p>
-              <IconContext.Provider value={{style: {fontSize: '30px', color:'#AAAAAA'}}}>
-                <FaFile style={{margin:'10 10 10 0'}}/>
-                <FaGithub style={{margin:'10px'}}/>
-                <FaLinkedin style={{margin:'10px'}}/>
-                <FaEnvelope style={{margin:'10px'}}/>
+              <IconContext.Provider value={{ style: { fontSize: '30px', color: '#AAAAAA' } }}>
+                <a href="">
+                  <FaFile style={{ margin: '10 10 10 0' }} />
+                </a>
+                <a href="https://github.com/IndushaS" target="_blank" rel="noopener noreferrer">
+                  <FaGithub style={{ margin: '10px' }} />
+                </a>
+                <a href="https://www.linkedin.com/in/indushasemba/" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin style={{ margin: '10px' }} />
+                </a>
+                <a href="mailto:im2semba@uwaterloo.ca" target="_blank" rel="noopener noreferrer">
+                  <FaEnvelope style={{ margin: '10px' }} />
+                </a>
+
+
+
+
 
               </IconContext.Provider>
-             
+
             </Col>
             <Col className="text-center" xs={12} md={{ span: 3, offset: 0 }}>
-      
-            <img height='250px' src={profile} /> 
+
+              <img height='250px' src={profile} />
             </Col>
           </Row>
         </Container>
